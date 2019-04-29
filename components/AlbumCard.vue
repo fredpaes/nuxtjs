ad14d6fc50ae3ee5998caee76b328f3db72a905a
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-04-29 18:49:49 -0500

    adding css fw

---
+++ components/AlbumCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="column is-one-quarter">
+    <div class="box">
+      <h3>{{ album.title }}</h3>
+      <a href="https://www.youtube.com/watch?v=DwMBu34rIOw" class="button is-primary">Ver álbum</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  padding: 1em;
+}
+h3 {
+  min-height: 5em;
+}
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,35 @@
 <template>
-  
+  <div class="container">
+    <h1 class="title">Álbums de la página:</h1>
+    <div class="columns is-multiline">
+      <album-card :album="album" v-for="album in albums" :key="album.id"/>
+    </div>
+    <a href="https://stories.devacademy.la/6-pasarelas-de-pago-que-debes-conocer-d5ae78f9369c">Pasarelas de pago</a>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import env from '../config/env';
+import AlbumCard from '../components/AlbumCard'
 export default {
-  
+  components: {
+    AlbumCard
+  },
+  data() {
+    return {
+      albums: []
+    }
+  },
+  created() {
+    axios.get(`${env.endpoint}/albums`)
+      .then(res => {
+        this.albums = res.data
+      })
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
-
-
